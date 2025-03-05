f4e598614c66a6b444a2b2fe9e3aebe2db3e3242
bilalmaq01 <bilalmaq01@gmail.com>
2025-03-05 17:06:06 -0600

    Finalized and added more books, List how many words are in the book and how much of each alphabetic letter are in the book

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from stats import numofwords
+from stats import numofletters
+from stats import sortnums
+import sys
 
 def get_book_text(book):
     with open(book) as f:
@@ -8,8 +11,23 @@ def get_book_text(book):
 
 
 def main():
-    path_to_file = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_file = sys.argv[1]
     text = get_book_text(path_to_file)
     totalnumwords = numofwords(text)
-    print (f"{totalnumwords} words found in the document")
+    letterdict = numofletters(text)
+    sorted = sortnums(letterdict)
+    print ("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {totalnumwords} total words")
+    print("--------- Character Count -------")
+    for char in sorted:
+        if char[0].isalpha() == True:
+            print(f"{char[0]}: {char[1]}")
+    print("============= END ===============")
+
+
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,24 @@ def numofwords(stringofbook):
     words = stringofbook.split()
     totalnumofwords = len(words)
     return totalnumofwords
+
+def numofletters(text):
+    lowertext = text.lower()
+    chars = {}
+    for char in lowertext:
+        if char not in chars:
+            chars[char] = 1
+        else:
+            chars[char] += 1
+    return chars
+
+def sortnums(dict):
+    listofnums = list(dict.items())
+    listofnums.sort(key=lambda tup: tup[1], reverse=True)
+    return listofnums
+
+
+
+
+
+    
